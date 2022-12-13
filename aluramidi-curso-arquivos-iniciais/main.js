70e73173b554e52b08bb226946f9ccd5db0a6859
leopoldoferreira <leopoldoferreira@live.com>
2022-12-13 17:31:27 -0300

    pt4

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,1 +1,19 @@
-document.querySelector('.tecla_pom');
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+//document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+// let i = 0;
+// while (i < listaDeTeclas.length) {
+for (let i = 0; i < listaDeTeclas.length; i++) {
+    const tecla = listaDeTeclas[i]
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){ //função anonima
+        tocaSom(idAudio)
+    }
+    //se while: i += 1
+}
