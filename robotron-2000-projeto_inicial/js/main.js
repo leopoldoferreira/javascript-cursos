80c51519a24c194350e9a0eca71bbf7fe39a93df
leopoldoferreira <leopoldoferreira@live.com>
2022-12-14 15:06:36 -0300

    sem comentário + json aula 05

--- robotron-2000-projeto_inicial/js/main.js
+++ robotron-2000-projeto_inicial/js/main.js
@@ -1,37 +1,46 @@
-// const subtrair = document.querySelector("#subtrair")
-// const somar = document.querySelector("#somar")
-// const braco = document.querySelector("#braco")
-
-// const controle = document.querySelectorAll(".controle-ajuste") //se alguém mudar html vai parar
-// por isso devemos usar data atributes
 const controle = document.querySelectorAll("[data-controle]");
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
 controle.forEach( (elemento) => {
     elemento.addEventListener("click", (evento) => {
-        // console.log(evento.target.textContent)
-        // manipulaDados(evento.target.textContent, evento.target.parentNode) //desacoplado js do html n faz sentido textcontent
         manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
 
     })
 })
 
-// somar.addEventListener("click", () => {
-//     braco.value = parseInt(braco.value) + 1;
-// })
-// subtrair.addEventListener("click", () => {
-//     braco.value = parseInt(braco.value) - 1;
-// })
-// somar.addEventListener("click", () => {manipulaDados("somar")})
-// subtrair.addEventListener("click", () => {manipulaDados("subtrair")})
-
-
-// function manipulaDados(operacao){
-//     if(operacao === "-") {
-//         braco.value = parseInt(braco.value) - 1
-//     } else {
-//         braco.value = parseInt(braco.value) + 1
-//     }
-// }
 function manipulaDados(operacao, controle){
     // const peca = controle.querySelector(".controle-contador")
     const peca = controle.querySelector("[data-contador]")
