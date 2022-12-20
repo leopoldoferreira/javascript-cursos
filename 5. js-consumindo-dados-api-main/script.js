4ccd44cddedd60dff8045f0b0e1a674782852b20
leopoldoferreira <leopoldoferreira@live.com>
2022-12-20 18:30:15 -0300

    curso 5 pt2, async+await

--- 5. js-consumindo-dados-api-main/script.js
+++ 5. js-consumindo-dados-api-main/script.js
@@ -1,5 +1,21 @@
-var consultaCEP = fetch("https://viacep.com.br/ws/01001000/json/")
-  .then((resposta) => resposta.json())
+async function buscaEndereco(cep) {
+    try {
+    var consultaCEP = fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    var consultaCEPConvertida = await (await consultaCEP).json()
+    if(consultaCEPConvertida.erro) {
+        throw Error('CEP não existente!');
+    }
+    console.log(consultaCEPConvertida)
+    return consultaCEPConvertida
+    } catch (erro) {
+        console.log(erro)
+    }
+}
+let ceps = ['01001000','01001001']
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores))
+Promise.all(conjuntoCeps).then(resposta => console.log(resposta))
+// buscaEndereco()
+/*  .then((resposta) => resposta.json())
   .then((r) => {
     if (r.erro) {
         throw Error('Esse cep não existe!')
@@ -8,4 +24,4 @@ var consultaCEP = fetch("https://viacep.com.br/ws/01001000/json/")
     }
   })
   .catch((erro) => console.log(erro))
-  .finally(mensagem => console.log('Processamento concluído'));
+  .finally(mensagem => console.log('Processamento concluído')); */
