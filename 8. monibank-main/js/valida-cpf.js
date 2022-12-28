4e9d941db71c60132ccc5a96d5fd920f00dd2c7c
leopoldoferreira <leopoldoferreira@live.com>
2022-12-28 18:23:36 -0300

    curso 8 pt3, valida idade

--- 8. monibank-main/js/valida-cpf.js
+++ 8. monibank-main/js/valida-cpf.js
@@ -1,6 +1,12 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    validaNumerosRepetidos(cpf);
+    if (validaNumerosRepetidos(cpf) ||
+    validaPrimeiroDigito(cpf) ||
+    validaSegundoDigito(cpf)){
+        console.log("cpf nao existe")
+    } else {
+        console.log("existe")
+    }
 }
 
 function validaNumerosRepetidos(cpf) {
@@ -16,4 +22,42 @@ function validaNumerosRepetidos(cpf) {
         '88888888888',
         '99999999999'
     ]
+    return numeroRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+    
+    if(soma == 10 || soma == 1) {
+        soma = 0;
+    }
+
+    return soma != cpf[9]
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+    
+    if(soma == 10 || soma == 1) {
+        soma = 0;
+    }
+
+    console.log(soma)
+    return soma != cpf[10]
 }
